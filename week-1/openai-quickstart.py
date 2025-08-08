71702e71e599ecb3145c0d44beb3e3db19d29c97
cybrtooth <cybrtooth@protonmail.com>
2025-08-07 18:10:14 -0600

    added openai api key and set up

--- week-1/openai-quickstart.py
+++ week-1/openai-quickstart.py
@@ -23,7 +23,7 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 # --------------------------------------------------------------
 
 response = client.responses.create(
-    model="gpt-4.1", input="Write a one-sentence bedtime story about a unicorn."
+    model="gpt-4o", input="Write a one-sentence bedtime story about a unicorn."
 )
 
 print(response.output_text)
